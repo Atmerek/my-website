112f9adfb2d55d9b734e726c5561b9e75772f2a6
The Atmerek <84788788+Atmerek@users.noreply.github.com>
2022-09-23 11:39:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const pc = document.querySelector("#pc");
+const SD = document.querySelector("#SD");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
+const pcContent = document.querySelector("#pc-content");
+const SDContent = document.querySelector("#SD-content");
+
+about.addEventListener("click", () => {
+  const aboutBox = new WinBox({
+    title: "About Me",
+    width: "400px",
+    height: "305px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+contact.addEventListener("click", () => {
+  const contactBox = new WinBox({
+    title: "Contact Me",
+    width: "400px",
+    height: "280px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+pc.addEventListener("click", () => {
+  const pcBox = new WinBox({
+    title: "My PC specs",
+    width: "400px",
+    height: "408px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: pcContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+SD.addEventListener("click", () => {
+  const SDBox = new WinBox({
+    title: "Stable Diffusion",
+    width: "400px",
+    height: "274px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: SDContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
